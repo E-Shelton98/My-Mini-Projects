30f91804e82f93149aff17f4c7d469feab28687d
Erik Shelton <erikshelton@Eriks-Air.lan1>
2021-01-01 12:04:31 -0500

    Added loading bar to Progress Steps

--- Progress Steps/script.js
+++ Progress Steps/script.js
@@ -1,7 +1,9 @@
 const progress = document.getElementById('progress')
+const progressBar = document.getElementById('progress-bar')
 const prev = document.getElementById('prev')
 const next = document.getElementById('next')
 const circles = document.querySelectorAll('.circle')
+const progressContainerBar = document.getElementById('progress-container-bar')
 
 let currentActive = 1
 
@@ -36,16 +38,27 @@ function update() {
 
   const actives = document.querySelectorAll('.active')
 
-  progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%'
+  progress.style.width =
+    ((actives.length - 1) / (circles.length - 1)) * 100 + '%'
 
-  if(currentActive === 1) {
-      prev.disabled = true
+  if (currentActive === 1) {
+    prev.disabled = true
+    progressBar.style.width = '0%'
+  } else if (currentActive === circles.length) {
+    next.disabled = true
+    progressBar.style.width = '100%'
+  } else {
+    prev.disabled = false
+    next.disabled = false
   }
-  else if (currentActive === circles.length) {
-      next.disabled = true
-  }
-  else {
-      prev.disabled = false
-      next.disabled = false
+
+  if (progressBar.style.width === '100%') {
+    let node = document.createElement('p')
+    let textNode = document.createTextNode('Progress Complete!')
+    node.appendChild(textNode)
+    node.classList.add("progress-statement")
+    
+    setTimeout(() => {progressContainerBar.appendChild(node)}, 4000)
+    
   }
 }
