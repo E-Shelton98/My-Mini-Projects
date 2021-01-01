ee451e49bc9b62099a2f1f728192c9ac5b17e7ca
Erik Shelton <erikshelton@Eriks-Air.lan1>
2020-12-31 22:40:07 -0500

    making Expanding Cards my own

--- Expanding Cards/script.js
+++ Expanding Cards/script.js
@@ -1,9 +1,10 @@
 const panels = document.querySelectorAll('.panel')
-
+const body = document.querySelector('body')
 panels.forEach((panel) => {
     panel.addEventListener('click', () => {
         removeActiveClasses()
         panel.classList.add('active')
+        body.style.backgroundImage = panel.style.backgroundImage
     })
 })
 
@@ -12,3 +13,7 @@ function removeActiveClasses() {
         panel.classList.remove('active')
     })
 }
+
+/* const container = document.querySelector('.container')
+
+container.style.backgroundImage = 'url("https://images.unsplash.com/photo-1531366936337-7c912a4589a7?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80")' */
