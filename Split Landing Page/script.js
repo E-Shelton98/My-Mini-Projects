21a159e1d07b371acf2b852f4303824d238a0563
Erik Shelton <erikshelton@Eriks-Air.lan1>
2021-01-11 21:51:04 -0500

    completed Form Wave Animation

--- Split Landing Page/script.js
+++ Split Landing Page/script.js
@@ -1,15 +1,28 @@
 const left = document.querySelector('.left')
 const right = document.querySelector('.right')
 const container = document.querySelector('.container')
+const leftBtn = document.getElementById('left-btn')
+const leftHolder = document.getElementById('left-holder')
+const rightBtn = document.getElementById('right-btn')
+const rightHolder = document.getElementById('right-holder')
 
 left.addEventListener('mouseenter', () => container.classList.add('hover-left'))
-left.addEventListener('mouseleave', () =>
-  container.classList.remove('hover-left')
-)
+left.addEventListener('mouseleave', () => {
+  container.classList.remove('hover-right')
+  leftHolder.classList.remove('active')
+})
+
+leftBtn.addEventListener('click', () => leftHolder.classList.add('active'))
+leftHolder.addEventListener('click', () => leftHolder.classList.remove('active'))
 
 right.addEventListener('mouseenter', () =>
   container.classList.add('hover-right')
 )
-right.addEventListener('mouseleave', () =>
+right.addEventListener('mouseleave', () => {
   container.classList.remove('hover-right')
+  rightHolder.classList.remove('active')
+})
+rightBtn.addEventListener('click', () => rightHolder.classList.add('active'))
+rightHolder.addEventListener('click', () =>
+  rightHolder.classList.remove('active')
 )
