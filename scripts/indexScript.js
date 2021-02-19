d969b4bda7f6807beec3f0a91c044dd36f0ab375
Erik Shelton <erikshelton@Eriks-Air.lan1>
2021-02-19 03:58:35 -0500

    relinked styling and scripts to html files,completed setup of project areas, need to add link back to home page and add pictures of projects.

--- scripts/indexScript.js
+++ scripts/indexScript.js
@@ -1,1 +1,12 @@
-const projectsArea = document.getElementById('projects-area')
+const projectLinkDivs = document.querySelectorAll('.link-div')
+const projectInfos = document.querySelectorAll('.project-info')
+
+projectLinkDivs.forEach((project, idx) => {
+  project.addEventListener('mouseover', () => {
+    showProjectInfo(idx)
+  })
+})
+
+function showProjectInfo(idx) {
+  projectInfos[idx].classList.toggle('show-info')
+}
